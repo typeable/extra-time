4d32a6342b63cdd86c4dd0574bbb1cfed03a4af5
Ville Tirronen <ville@typeable.io>
2023-04-11 14:58:35 +0300

    exterminate orphans

--- src/Data/Time/Holidays.hs
+++ src/Data/Time/Holidays.hs
@@ -8,15 +8,14 @@ import qualified Data.Set as S
 import           Data.Time
 import           Data.Time.Calendar.OrdinalDate
 import           Data.Time.LocalTime.TimeZone.Series
-import           Data.Csv (Only(..))
 import           Data.FileEmbed (makeRelativeToProject)
 import           Data.Csv.Embed (embedRecords)
-import           Holidays.Orphans ()
+import           Holidays.EDay (eDayToDay, EDay)
 
 opmHolidays2023to2030 :: S.Set Day
 opmHolidays2023to2030 = S.fromList $
-  fromOnly <$>
-    $(makeRelativeToProject "data/opm-holidays.csv" >>= embedRecords (Proxy @(Only Day)))
+  eDayToDay <$>
+    $(makeRelativeToProject "data/opm-holidays.csv" >>= embedRecords (Proxy @(EDay)))
 
 addTimeExcludingHolidays
   :: S.Set Day
